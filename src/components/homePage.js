28aeb7cf93bec4984d6567c2bbb2239bd31d7b5d
nikhil bhatia <nikhilbhatia2004@gmail.com>
2019-05-05 10:31:22 +0300

    form boiler plate

---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavigationBar from './navbar'
+import Form from './form'
+
+const HomePage = (props) => {
+return (
+  <div>
+  <NavigationBar />
+  <Form />
+  </div>
+)
+}
+
+export default HomePage
